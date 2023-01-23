66c84f4777aef1f5ef94d2b34b3701fbcbb52224
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-01-23 12:50:30 +0300

    gh-pages;

--- up.sh
+++
@@ -1,8 +0,0 @@
-echo provide your GitHub token
-read github_token
-echo provide your Todoist token
-read todoist_token
-export GITHUB_TOKEN=${github_token}
-export TODOIST_TOKEN=${todoist_token}
-gradle clean build
-docker compose up -d --build
